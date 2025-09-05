b6a56804cb4780db5479fa253cfb8704100e1524
Leon Schoorl <leon@qbaylogic.com>
2025-09-05 18:05:31 +0200

    Remove template-haskell package dep

--- src/ModC.hs
+++ src/ModC.hs
@@ -1,9 +1,8 @@
-{-# LANGUAGE TemplateHaskellQuotes #-}
 module ModC where
 
-import Language.Haskell.TH
 import ModD
 
-
-someThThingy :: DecsQ
+someThThingy :: Monad m => m [a]
+-- ^ would normally be: Q [Dec]
+-- But with this more generic type, we don't have to depend on template-haskell
 someThThingy = return []
